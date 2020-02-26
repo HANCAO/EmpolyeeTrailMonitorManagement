b4b2f1791f852192303cc1840d6045df13ff12b1
HANCAO <helpsmileycaesar@foxmail.com>
2020-02-26 14:31:36 +0800

    Add Database table Options

---
+++ dbo.EmpolyeeTrail.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[EmpolyeeTrail] (
+    [Id]             BIGINT            IDENTITY (1, 1) NOT NULL,
+    [GPSX]           FLOAT           NULL,
+    [GPSY]           FLOAT           NULL,
+    [BmapLap]        FLOAT           NULL,
+    [BmapLng]        FLOAT           NULL,
+    [CreateTime]     DATETIME  NULL DEFAULT (getdate()),
+    [CreateUser]     VARCHAR(20) NULL,
+    [IsCar]          BIT            NULL DEFAULT ((0)),
+    [Distance]       INT            NULL,
+    [DistanceSecond] INT            NULL,
+    CONSTRAINT [PK_EmpolyeeTrail] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
