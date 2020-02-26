7b44369165e47f6b3672d27aa602d5e3686fcfb0
HANCAO <helpsmileycaesar@foxmail.com>
2020-02-26 16:27:22 +0800

    Add View Component about Empolyee and Data count

---
+++ EmpolyeeTrailMonitor/ViewComponents/EmployeeCountViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using EmpolyeeTrailMonitor.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpolyeeTrailMonitor.ViewComponents
+{
+    public class EmployeeCountViewComponent:ViewComponent
+    {
+        private readonly EmpolyeeTrailContext empolyeeTrails;
+        public EmployeeCountViewComponent(EmpolyeeTrailContext empolyeeTrails)
+        {
+            this.empolyeeTrails = empolyeeTrails;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //获取数据总行数
+            ViewData["DataCount"] = empolyeeTrails.EmpolyeeTrail.Count();
+            //获取员工个数
+            ViewData["EmpolyeeCount"] = empolyeeTrails.EmpolyeeTrail.Select(s => s.CreateUser).Distinct().Count();
+
+            return View();
+        }
+    }
+}
